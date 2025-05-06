ffeb4b06a54f0d71442f8eb22d9784d8ebaedbf0
VioletHannah <2487289554@qq.com>
2025-05-06 23:53:37 +0800

    1.metric损失添加权重 2.eval适应于多声源

--- eval.py
+++ eval.py
@@ -2,18 +2,20 @@
 # -*- coding: utf-8 -*-
 # @Time : 2025-03-24 12:44
 # @Author : 箴澄
-# @Site :
+# @Site : 评估model文件好不好，在测试集上运行代码，看看效果
 # @File : eval.py
 # @Software: PyCharm
 from load_data import AudioDoADataset
-from max_corr_backbone import SoundDetBackbone
-from train import AngleLoss
+from train import heatmapLoss
+from MultiSource_3DCNN_mapNet import MultiSource3DCNNMapNet
+from logger import *
 
 from torch.utils.data import DataLoader
 import torch
 import matplotlib.pyplot as plt
 import numpy as np
 import math
+import cv2
 
 def plot_polar_heatmap(azimuth_errors, elevation_errors, title=""):
     """
@@ -31,19 +33,28 @@ def plot_polar_heatmap(azimuth_errors, elevation_errors, title=""):
     return ax
 
 
-def calculate_spatial_angle(azimuth_label, elevation_label, azimuth_true, elevation_true):
-    # 将标签方向转换为三维向量
-    x1 = math.cos(azimuth_label) * math.cos(elevation_label)
-    y1 = math.sin(azimuth_label) * math.cos(elevation_label)
-    z1 = math.sin(elevation_label)
+def transfer_to_vector(azimuth, elevation):
+    """
+    将方位角和俯仰角转换为三维向量
+    参数：
+    - azimuth: 方位角（弧度）
+    - elevation: 俯仰角（弧度）
+    返回：
+    - 三维向量
+    """
+    x = math.cos(azimuth) * math.cos(elevation)
+    y = math.sin(azimuth) * math.cos(elevation)
+    z = math.sin(elevation)
+    return np.array([x, y, z])
 
-    # 将真实方向转换为三维向量
-    x2 = math.cos(azimuth_true) * math.cos(elevation_true)
-    y2 = math.sin(azimuth_true) * math.cos(elevation_true)
-    z2 = math.sin(elevation_true)
+
+def calculate_spatial_angle(azimuth_label, elevation_label, azimuth_true, elevation_true):
+    # 将标签和真实方向转换为三维向量
+    label_vector = transfer_to_vector(azimuth_label, elevation_label)
+    true_vector = transfer_to_vector(azimuth_true, elevation_true)
 
     # 计算点积
-    dot_product = x1 * x2 + y1 * y2 + z1 * z2
+    dot_product = label_vector @ true_vector
 
     # 确保点积在有效范围内
     dot_product = max(min(dot_product, 1.0), -1.0)
@@ -57,26 +68,65 @@ def calculate_spatial_angle(azimuth_label, elevation_label, azimuth_true, elevat
 def evaluate_model(dataset_path, model_path='sound_model.pth'):
     # 1. 加载模型
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = SoundDetBackbone().to(device)
-    checkpoint = torch.load(model_path)
+    model = MultiSource3DCNNMapNet().to(device)
+    checkpoint = torch.load(model_path, weights_only=True)
     model.load_state_dict(checkpoint['model_state_dict'])
-    model.eval()
+    model.train()
 
     # 2. 加载测试数据集
-    test_dataset = AudioDoADataset(root_dir=dataset_path, split="test")
+    test_dataset = AudioDoADataset(root_dir=dataset_path, split="test", n_channels=64)
     test_loader = DataLoader(test_dataset, batch_size=16, shuffle=False)
 
     # 3. 初始化存储
-    all_azimuth_errors = []
-    all_elevation_errors = []
-    total_loss = 0.0
+    # true_azimuth = []
+    # true_elevation = []
+    # pred_azimuth = []
+    # pred_elevation = []
 
-    with torch.no_grad():
+    with (torch.no_grad()):
         for x, y in test_loader:
-            x, y = x.to(device), y.to(device)
+            x = x.to(device)
+            y = y.to(device)
+            pred = model(x)
+            loss = heatmapLoss(pred, y)
+            logger.info(f"Test Loss: {loss.item():.6f}")
+
+            row_num = 4
+            col_num = 4
+            row_pred = []
+            row_gt = []
+            for h in range(row_num):
+                col_pred = []
+                col_gt = []
+                for w in range(col_num):
+                 pred_slice = pred[h * col_num + w].cpu().detach().numpy()
+                 heatmap_slice = y[h * col_num + w].cpu().detach().numpy()
+                 pred_slice = np.pad(pred_slice[2:-2, 2:-2], ((2, 2), (2, 2)), 'constant', constant_values=1)
+                 heatmap_slice = np.pad(heatmap_slice[2:-2, 2:-2], ((2, 2), (2, 2)), 'constant', constant_values=1)
+                 col_pred.append(pred_slice)
+                 col_gt.append(heatmap_slice)
+                row_pred.append(np.concatenate(col_pred, axis=1))
+                row_gt.append(np.concatenate(col_gt, axis=1))
+            result = np.concatenate(row_pred, axis=0)
+            gt = np.concatenate(row_gt, axis=0)
+            result_uint8 = (result * 255).astype('uint8')
+            cv2.imwrite("evalresult.png", result_uint8)
+            gt_uint8 = (gt * 255).astype('uint8')
+            cv2.imwrite("evalgt.png", gt_uint8)
+
+            # true_azimuth.extend(y[:, 0].cpu().numpy().tolist())
+            # true_elevation.extend(y[:, 1].cpu().numpy().tolist())
 
             # 预测和计算误差
-            pred = model(x)
+            # pred_azimuth.extend(pred[:, 0].cpu().numpy().tolist())
+            # pred_elevation.extend(pred[:, 1].cpu().numpy().tolist())
+
+
+    # plot_joint_error_heatmap(true_azimuth, true_elevation, pred_azimuth, pred_elevation)
+
+
+"""
+
             loss = AngleLoss(pred, y)
             total_loss += loss.item()
 
@@ -92,31 +142,34 @@ def evaluate_model(dataset_path, model_path='sound_model.pth'):
             all_azimuth_errors.extend(azimuth_deg_errors.cpu().numpy())
             all_elevation_errors.extend(elevation_deg_errors.cpu().numpy())
 
+
     # 4. 打印统计信息
     print(f"Test Loss: {total_loss / len(test_loader):.4f}")
     print(f"Azimuth MAE: {np.mean(all_azimuth_errors):.2f}° ± {np.std(all_azimuth_errors):.2f}°")
     print(f"Elevation MAE: {np.mean(all_elevation_errors):.2f}° ± {np.std(all_elevation_errors):.2f}°")
-
+"""
     # 5. 可视化误差分布
-    plt.figure(figsize=(12, 5))
 
-    plt.subplot(121)
-    plot_polar_heatmap(all_azimuth_errors, all_elevation_errors,
-                       "方位角-俯仰角联合误差分布")
 
-    plt.subplot(122)
-    plt.hist2d(all_azimuth_errors, all_elevation_errors,
-               bins=(30, 20), cmap='viridis')
-    plt.colorbar(label='样本数量')
-    plt.xlabel('方位角误差 (°)')
-    plt.ylabel('俯仰角误差 (°)')
-    plt.title("二维直方图误差分布")
-
-    plt.tight_layout()
-    plt.savefig('error_analysis.png')
-    plt.show()
+    # plt.figure(figsize=(12, 5))
+    #
+    # plt.subplot(121)
+    # plot_polar_heatmap(all_azimuth_errors, all_elevation_errors,
+    #                    "方位角-俯仰角联合误差分布")
+    #
+    # plt.subplot(122)
+    # plt.hist2d(all_azimuth_errors, all_elevation_errors,
+    #            bins=(30, 20), cmap='viridis')
+    # plt.colorbar(label='样本数量')
+    # plt.xlabel('方位角误差 (°)')
+    # plt.ylabel('俯仰角误差 (°)')
+    # plt.title("二维直方图误差分布")
+    #
+    # plt.tight_layout()
+    # plt.savefig('error_analysis.png')
+    # plt.show()
 
 
 if __name__ == "__main__":
-    evaluate_model(dataset_path="/home/zengkehan/voice/single64_dataset",
-                   model_path="sound_model.pth")
+    evaluate_model(dataset_path="/home/zengkehan/voice/multisource_dataset",
+                   model_path="/home/zengkehan/ssl/mulsource_sound_model91.pth")
--- metric.py
+++ metric.py
@@ -1,10 +1,33 @@
 import torch
-def heatmapLoss(pred, target):
+def heatmapBiLoss(pred, target, threshold=0.5, alpha=20):
+    # 二分类loss pred: [B, 128, 128]，target: [B, 128, 128]
+    # 计算二分类交叉熵损失
+    loss = 0
+    for i in range(pred.shape[0]):
+        pred_i = pred[i].unsqueeze(0)
+        target_i = target[i].unsqueeze(0)
+        # 计算二分类交叉熵损失
+        bce_loss = torch.nn.BCELoss()(pred_i, target_i)
+        # 计算加权损失
+        weight = torch.where(target_i > threshold, alpha, 1.0)
+        err = (pred_i - target_i) ** 2
+        weighted_loss = (err * weight).sum() / pred_i.shape[0]
+        # 将加权损失与二分类交叉熵损失相加
+        loss += bce_loss + weighted_loss
+    # 返回平均损失
+    return loss / pred.shape[0]
+
+
+def heatmapLoss(pred, target, threshold=0.5, alpha=20):
     # pred: [B, 128, 128]，target: [B, 128, 128]
-    mse = torch.nn.MSELoss()
-    # 计算均方误差损失
-    loss = mse(pred, target)
-    return torch.mean(loss)
+    weight = torch.where(target > threshold, alpha, 1.0)
+    err = (pred - target) ** 2
+    return (err * weight).sum() / pred.shape[0]
+
+    # mse = torch.nn.MSELoss()
+    # # 计算均方误差损失
+    # loss = mse(pred, target)
+    # return torch.mean(loss)
 
 #!
 def AngleLoss(pred, target):
